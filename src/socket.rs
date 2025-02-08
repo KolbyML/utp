b53a2a0df49642f06c9ba906518909679c4860e3
Kolby Moroz Liebl <31669092+KolbyML@users.noreply.github.com>
2025-02-08 12:49:38 -0700

    Update socket.rs

--- src/socket.rs
+++ src/socket.rs
@@ -111,7 +111,7 @@ where
                             .get(&acc_cid)
                             .or_else(|| conns.get(&we_init_cid))
                             .or_else(|| conns.get(&peer_init_cid));
-                        warn!("conn: {:?} {:?}", conn, packet);
+                        warn!("conn: {:?} {:?} {:?}", conn, packet, awaiting);
                         match conn {
                             Some(conn) => {
                                 let _ = conn.send(StreamEvent::Incoming(packet));
