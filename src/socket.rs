0508bb524ca14f813bcdc85f66948c97afe42017
Kolby Moroz Liebl <31669092+KolbyML@users.noreply.github.com>
2025-02-08 13:53:56 -0700

    Update socket.rs

--- src/socket.rs
+++ src/socket.rs
@@ -159,7 +159,7 @@ where
                                         incoming_conns.insert(cid, (peer, packet));
                                     }
                                 } else {
-                                    tracing::debug!(
+                                    tracing::error!(
                                         cid = %packet.conn_id(),
                                         packet = ?packet.packet_type(),
                                         seq = %packet.seq_num(),
