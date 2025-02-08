a24574dbaad9f86673706c55535a829566a1ebc2
Kolby Moroz Liebl <31669092+KolbyML@users.noreply.github.com>
2025-02-08 16:46:40 -0700

    Update conn.rs

--- src/conn.rs
+++ src/conn.rs
@@ -1049,7 +1049,7 @@ impl<const N: usize, P: ConnectionPeer> Connection<N, P> {
     }
 
     fn on_reset(&mut self) {
-        tracing::error!("RESET from remote {:?}", self.state);
+        tracing::error!("RESET from remote {:?} {:?}", self.state, self.cid);
 
         // If the connection is not already closed or reset, then reset the connection.
         if !std::matches!(self.state, State::Closed { .. }) {
